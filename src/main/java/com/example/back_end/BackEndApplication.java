22f17f01ea147a3b58657c54bbb8728560be0b35
Kaveesha Weerakoon <savindukaveesha11@gmail.com>
2024-06-11 22:34:42 +0530

    First Commit

--- src/main/java/com/example/back_end/BackEndApplication.java
+++ src/main/java/com/example/back_end/BackEndApplication.java
@@ -3,11 +3,14 @@ package com.example.back_end;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class BackEndApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
+		System.out.println("SD");
 	}
 
 }
