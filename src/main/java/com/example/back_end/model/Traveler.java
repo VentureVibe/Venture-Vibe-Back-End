bcf48219d4a205440d90b4ac0d7e0a582cb2beab
Kaveesha Weerakoon <savindukaveesha11@gmail.com>
2024-07-19 23:23:37 +0530

    Creating Travel Plan and Traveler Back end

--- src/main/java/com/example/back_end/model/Traveler.java
+++ src/main/java/com/example/back_end/model/Traveler.java
@@ -1,12 +1,14 @@
 package com.example.back_end.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,13 +20,13 @@ public class Traveler {
     @Id
     private String id;
 
-    @Column(length = 320,nullable = false)
+    @Column(length = 320, nullable = false)
     private String email;
 
     @Column(length = 255)
     private String name;
 
-    @Column(length = 255,nullable = false)
+    @Column(length = 255, nullable = false)
     private String password;
 
     @Column(length = 2048)
@@ -32,5 +34,14 @@ public class Traveler {
 
     @Column(length = 2048)
     private String coverImg;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "traveler_travelplan",
+            joinColumns = @JoinColumn(name = "traveler_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "travelplan_id", referencedColumnName = "id")
+    )
+
+    private List<TravelPlan> travelplans = new ArrayList<>();
 
 }
