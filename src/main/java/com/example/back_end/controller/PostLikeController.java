8442f8664f12c4cf57dd9b4bcb12d6d9ec9761f6
Rashmika Guruge <rashmikaguruge01@gmail.com>
2024-07-26 00:18:20 +0530

    add like

--- src/main/java/com/example/back_end/controller/PostLikeController.java
+++ src/main/java/com/example/back_end/controller/PostLikeController.java
@@ -9,10 +9,7 @@ import com.example.back_end.service.PostLikeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value="/api/v1/likes")
@@ -25,6 +22,18 @@ public class PostLikeController {
     public ResponseEntity<PostLikeDTO> createPostLike(@RequestBody PostLike postLike) {
         PostLikeDTO createdPostLike = postLikeService.addPostLike(postLike);
         return new ResponseEntity<>(createdPostLike, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/{postId}/{travelerId}")
+    public ResponseEntity<PostLikeDTO> deletePostLike(@PathVariable Integer postId, @PathVariable String travelerId) {
+        PostLikeDTO deletedPostLike = postLikeService.deletePostLike(postId, travelerId);
+        return new ResponseEntity<>(deletedPostLike, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{postId}/{travelerId}")
+    public ResponseEntity<Boolean> isPostLiked(@PathVariable Integer postId, @PathVariable String travelerId) {
+        boolean isLiked = postLikeService.isPostLiked(postId, travelerId);
+        return new ResponseEntity<>(isLiked, HttpStatus.OK);
     }
 
 }
