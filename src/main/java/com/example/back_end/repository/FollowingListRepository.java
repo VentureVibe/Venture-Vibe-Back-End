d2d12c2d2ee7838f3fe192ebdb452eec19f8fe82
Rashmika Guruge <rashmikaguruge01@gmail.com>
2024-07-29 11:16:23 +0530

    add chat

--- src/main/java/com/example/back_end/repository/FollowingListRepository.java
+++ src/main/java/com/example/back_end/repository/FollowingListRepository.java
@@ -7,7 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface FollowingListRepository extends JpaRepository<FollowingList, Long> {
-    //Optional<FollowingList> findByTravelerId(Long travelerId);
 
     Optional<FollowingList> findByTraveler(Traveler traveler);
+
+    Optional<FollowingList> findByTravelerId(String travelerId);
 }
