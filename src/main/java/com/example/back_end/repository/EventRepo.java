b0e4f6f275a8be626284b6dceeae81a14751c273
AkilaBuwaneka <akilamg2000@gmail.com>
2024-07-30 15:30:14 +0530

    update

---
+++ src/main/java/com/example/back_end/repository/EventRepo.java
@@ -0,0 +1,12 @@
+package com.example.back_end.repository;
+
+import com.example.back_end.model.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepo extends JpaRepository<Event, Integer> {
+    Page<Event> findAll(Pageable pageable);
+}
