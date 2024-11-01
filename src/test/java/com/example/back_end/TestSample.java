abfdfe3da7df60e84379abe457051324b9f3f3a2
pavanwije <108750186+pavanwije@users.noreply.github.com>
2024-11-01 18:41:43 +0530

    Initial Setup and Constants

---
+++ src/test/java/com/example/back_end/TestSample.java
@@ -0,0 +1,11 @@
+// WarehouseProductivity.java
+public class WarehouseProductivity {
+    private static final String APP_NAME = "Warehouse Productivity Monitoring";
+    private static final String VERSION = "1.0.0";
+    private static double productivityScore = 0.0;
+
+    public static void main(String[] args) {
+        System.out.println("App Name: " + APP_NAME);
+        System.out.println("Version: " + VERSION);
+    }
+}
