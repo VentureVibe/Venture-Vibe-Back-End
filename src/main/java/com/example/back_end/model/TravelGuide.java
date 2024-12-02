5429f8c498ba85f990f229d94d09ade13cf32caf
AkilaBuwaneka <akilamg2000@gmail.com>
2024-12-02 21:47:57 +0530

    added new files

--- src/main/java/com/example/back_end/model/TravelGuide.java
+++ src/main/java/com/example/back_end/model/TravelGuide.java
@@ -1,4 +1,4 @@
-package com.example.back_end.model;
+/*package com.example.back_end.model;
 
 import com.example.back_end.dto.ExperienceDTO;
 import com.fasterxml.jackson.annotation.JsonBackReference;
@@ -20,5 +20,33 @@ public class TravelGuide extends ServiceProvider {
 
     @OneToMany(mappedBy = "travelGuide", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
+    private List<TravelGuideServices> travelGuideServices;
+}*/
+
+// src/main/java/com/example/back_end/model/TravelGuide.java
+package com.example.back_end.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class TravelGuide extends ServiceProvider {
+    private Float radius;
+    private Double price;
+    @ElementCollection
+    private List<String> specialties;
+    @ElementCollection
+    private List<String> languages;
+
+    @OneToMany(mappedBy = "travelGuide", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Experience> experiences;
+
+    @OneToMany(mappedBy = "travelGuide", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<TravelGuideServices> travelGuideServices;
 }
