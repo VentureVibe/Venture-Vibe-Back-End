ae5b674b0c08375a3c24d9f68ad87a68360b872b
Randika-BLS <sithumrandika85@gmail.com>
2024-12-03 20:52:59 +0530

    change

--- src/main/java/com/example/back_end/controller/SubscriberController.java
+++ src/main/java/com/example/back_end/controller/SubscriberController.java
@@ -1,25 +1,34 @@
 package com.example.back_end.controller;
 
 import com.example.back_end.dto.SubscriberDTO;
+import com.example.back_end.model.EmailSubscriber;
+import com.example.back_end.repository.SubscriberRepository;
 import com.example.back_end.service.SubscriberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/subscribers")
+@CrossOrigin("http://localhost:5173")
 public class SubscriberController {
 
     @Autowired
     private SubscriberService subscriberService;
 
+    @Autowired
+    private SubscriberRepository subscriberRepository;
+
     @PostMapping
     public ResponseEntity<String> subscribe(@RequestBody SubscriberDTO subscriberDTO){
         subscriberService.saveSubscriber(subscriberDTO.getEmail());
         return ResponseEntity.ok("Subscribed successfully");
+    }
+    @GetMapping
+    public List<EmailSubscriber> getAllSubscribers() {
+        return subscriberRepository.findAll();
     }
 
 }
--- src/main/java/com/example/back_end/service/SubscriberService.java
+++ src/main/java/com/example/back_end/service/SubscriberService.java
@@ -1,8 +1,12 @@
 package com.example.back_end.service;
 import com.example.back_end.model.EmailSubscriber;
+import com.example.back_end.model.Traveler;
 import com.example.back_end.repository.SubscriberRepository;
+import com.example.back_end.repository.TravelerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SubscriberService {
@@ -10,7 +14,12 @@ public class SubscriberService {
     @Autowired
     private SubscriberRepository subscriberRepository;
 
+    @Autowired
+    private TravelerRepo travelerRepo;
+
+
     public void saveSubscriber(String email) {
+
         EmailSubscriber subscriber = new EmailSubscriber();
         subscriber.setEmail(email);
         subscriberRepository.save(subscriber);
